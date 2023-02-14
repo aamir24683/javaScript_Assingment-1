cdfe01fe8eda509ac4988b8255c8aa705fba7f4b
Aamir Shahzad <56902801+aamir24683@users.noreply.github.com>
2023-02-14 21:28:36 +0500

    This is my first Assignment in javascript
    the first assignment is done

---
+++ question 1.js
@@ -0,0 +1,9 @@
+let x = 12;
+let y = 10;
+if (x > y) {
+  console.log("X is greater then B" + "<br>");
+} else if (y > x) {
+  console.log("Y is greater then A" + "<br>");
+} else {
+  console.log("X and Y is equal" + "<br>");
+}
---
+++ question 2.js
@@ -0,0 +1,12 @@
+let a = 40;
+let b = 51;
+let c = 61;
+if (a > b && e > c) {
+  console.log("A is greater then B and C" + "<br>");
+} else if (b > a && b > c) {
+  console.log("B is greater then A and C" + "<br>");
+} else if (c > a && c > b) {
+  console.log("C is greater then B and A" + "<br>");
+} else {
+  console.log("enter a valid input" + "<br>");
+}
